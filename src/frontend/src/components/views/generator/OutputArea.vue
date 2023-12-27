c90edc9ad684cccbc17fea803f246655401d2b59
shinokiwa <shinokiwa@gmail.com>
2023-12-27 17:21:22 +0900

    フォーカス処理など

--- src/frontend/src/components/views/generator/OutputArea.vue
+++ src/frontend/src/components/views/generator/OutputArea.vue
@@ -2,7 +2,7 @@
 /**
  * 生成ビューの出力エリア
  */
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, watch} from 'vue'
 import WindowArea from '@/components/elements/WindowArea.vue'
 
 import { useTemp } from '@/composables/api/res/output/temp'
@@ -16,38 +16,81 @@ onUnmounted(()=>{
     close()
 })
 
-const isSelected = ref(false)
+const selectImgs = ref([])
+const lastSelectIndex = ref(null)
 
-function toggleSelect (id) {
-    let selected = false
-    imageList.value.forEach(image => {
-        if (image.id === id) {
-            image.selected = !image.selected
-        }
+function selectItem (event, index, image) {
+    event.preventDefault()
+    event.stopPropagation()
+    // Ctrlキーが押されていない場合は選択状態を解除
+    if (!event.ctrlKey) {
+        selectImgs.value = []
+    }
 
-        if (image.selected) {
-            selected = true
+    // Shiftキーが押されている場合は範囲選択
+    if (event.shiftKey && lastSelectIndex.value !== null) {
+        const range = [lastSelectIndex.value, index].sort()
+        imageList.value.forEach((v, i) => {
+            if (i >= range[0] && i <= range[1]) {
+                selectImgs.value.push(v.url)
+            }
+        })
+    } else {
+        if (selectImgs.value.includes(image.url)) {
+            selectImgs.value = selectImgs.value.filter(url => url !== image.url)
+        } else {
+            selectImgs.value.push(image.url)
         }
-    })
-    isSelected.value = selected
+        lastSelectIndex.value = index
+    }
+
+    return false
 }
+
+function clearSelect () {
+    selectImgs.value = []
+    lastSelectIndex.value = null
+}
+
+
+function keyDown (event) {
+    if (event.key === 'ArrowRight') {
+        const nextItem = event.target.nextElementSibling
+        if (nextItem) {
+            nextItem.focus()
+            nextItem.click()
+        }
+    } else if (event.key === 'ArrowLeft') {
+        const prevItem = event.target.previousElementSibling
+        if (prevItem) {
+            prevItem.focus()
+            prevItem.click()
+        }
+    }
+}
+
 </script>
 
 <template>
 <WindowArea window-title="出力画像">
     <div class="menu">
         <button @click="refresh">更新</button>
-        <button v-if="isSelected" @click="deleteSelected">選択したものを削除</button>
+        <button v-if="selectImgs.length > 0" @click="deleteSelected">選択したものを削除</button>
         <button @click="deleteAll">すべて削除</button>
     </div>
-    <div class="gallery">
+    <div class="gallery"
+        @click="clearSelect"
+    >
         <a
             class="gallery-item"
-            v-for="image in imageList"
+            v-for="(image, index) in imageList"
             :key="image.id"
-            :class="{ selected: image.selected }"
-            href="#"
-            @click="toggleSelect(image.id)"
+            :class="{ selected: selectImgs.includes(image.url) }"
+            :href="'output/temp/' + image.url + '?t=' + image.timestamp"
+            target="_blank"
+
+            @click="selectItem($event, index, image)"
+            @keydown="keyDown"
         >
             <img
                 :src="'output/temp/' + image.url + '?t=' + image.timestamp"
@@ -88,8 +131,8 @@ function toggleSelect (id) {
 }
 
 .gallery-item:focus {
-    outline: 1px solid #333333;
-    background-color: #3344cc;
+    outline: 0;
+    box-shadow: 0 0 0 2px #333333;
 }
 
 .gallery-item.selected {
